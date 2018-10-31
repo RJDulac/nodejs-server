6f55e5246a8d1f5ce907aebc1e537006f5dda9c7
Ryan Dulac <analyticrye@gmail.com>
2018-10-31 03:46:18 -0400

    server takes input and puts the text into a file

--- app.js
+++ app.js
@@ -1,9 +1,42 @@
 //import function from nodejs -importing global http module
 const http = require('http');
+const fs = require('fs');
 
 //event driven architecture -executes function when we get server requests
 const server = http.createServer((req,res) => {
-    console.log(req.url, req.method, req.headers);
+    const url = req.url;
+    const method = req.method;
+    if (url === '/') {
+        res.write('<html>');
+        res.write('<head><title>Enter Message</title></head>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+        res.write('</html>');
+        return res.end();
+    }
+    if (url === '/message' && method === 'POST') {
+        const body = [];
+        //listen on data event
+        req.on('data',(chunk)=> {
+        console.log(chunk);
+            body.push(chunk);
+        });
+        //fired when done parsing incoming data from stream
+        req.on('end',() =>{
+            //all chunks are in an array and turned into a string
+            const parsedBody = Buffer.concat(body).toString();
+            console.log(parsedBody);
+            const message = parsedBody.split('=')[1];
+            fs.writeFileSync('message.text', message);
+        });
+        //write file
+        // fs.writeFileSync('message.text', 'Dummy Text');
+        //redirect user
+        res.statusCode = 302;
+        //where we redirect user
+        res.setHeader('Location', '/');
+        //use return to avoid running html code
+        return res.end();
+    }
     //ends server - hard exists event loop
     // process.exit();
     //tell it's html
